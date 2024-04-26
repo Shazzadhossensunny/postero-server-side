be179dbbfb0d57d490b35e5842139bb3d20aa9f3
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-04-27 03:46:02 +0600

    email api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.POSTERO_USER}:${process.env.POSTERO_PASS}@cluster0.5kgqkgx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -35,6 +36,21 @@ async function run() {
       res.send(result)
 
     })
+
+    app.get('/items/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await itemCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/myProduct/:email', async(req, res)=>{
+      const result =await itemCollection.find({email:req.params.email}).toArray()
+      res.send(result)
+
+
+    })
+
 
     app.post('/items', async(req, res)=>{
       const item = req.body;
